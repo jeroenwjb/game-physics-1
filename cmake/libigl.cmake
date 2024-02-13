0f5804ab79d74e586186e7afa08e00ccc72048e0
Leonidas Venetis <leonidasvenetis@gmail.com>
2024-02-13 13:58:36 +0100

    first upload

---
+++ cmake/libigl.cmake
@@ -0,0 +1,11 @@
+if(TARGET igl::core)
+    return()
+endif()
+
+include(FetchContent)
+FetchContent_Declare(
+    libigl
+    GIT_REPOSITORY https://github.com/libigl/libigl.git
+    GIT_TAG main
+)
+FetchContent_MakeAvailable(libigl)
